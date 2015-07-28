2eb0601325d455f9fe5682ce29a4e0ec61ffc54a
Ryan J. Froehlich <ryan@synergy-cns.com>
2015-07-28 17:22:57 -0500

    Add icons, stream views, and playback

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'ionic.ion.imageCacheFactory'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -35,6 +35,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     url: '/',
     templateUrl: 'templates/views/index.html',
     controller: 'IndexCtrl'
+  })
+  .state('stream', {
+      url: '/stream/:streamId',
+      templateUrl: 'templates/views/stream.html',
+      controller: 'StreamCtrl'
   });
 
   // if none of the above states are matched, use this as the fallback
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,17 @@
 angular.module('starter.controllers', [])
-    .controller('IndexCtrl', function($scope) {
+    .controller('IndexCtrl', function($scope, $ImageCacheFactory, $cordovaSplashscreen) {
 
         console.log('IndexCtrl');
-        $scope.isPlaying = false;
+
+        $ImageCacheFactory.Cache([
+            'img/bg_main_lg.png',
+            'img/btn_stream1_lg.png',
+            'img/btn_stream2_lg.png',
+            'img/btn_stream3_lg.png'
+        ]).then(function() {
+            console.log('ALL IMAGES LOADED!');
+            $cordovaSplashscreen.hide();
+        });
 
         // Handler for Stream 1 Play Button
         $scope.streamHD1onClick = function () {
@@ -12,7 +21,6 @@ angular.module('starter.controllers', [])
                 var audStreamHD1 = new Audio('http://peridot.streamguys.com:5350/iheartclassical');
                 audStreamHD1.addEventListener("playing", function() {
                     console.log('Playback started!');
-                    $scope.isPlaying = true;
                 });
 
                 audStreamHD1.play();
@@ -23,5 +31,46 @@ angular.module('starter.controllers', [])
             }
 
         }
+
+    })
+    .controller('StreamCtrl', function($scope, $stateParams, $rootScope) {
+
+        console.log('StreamCtrl: ' + $stateParams.streamId);
+
+        var objStreamURLs = {
+            1: 'http://peridot.streamguys.com:5350/iheart',
+            2: 'http://peridot.streamguys.com:5350/iheartjazz',
+            3: 'http://peridot.streamguys.com:5350/iheartclassical'
+        };
+
+        if($rootScope.audAudioPlayer) {
+
+            console.log($rootScope.audAudioPlayer.src);
+
+            if($rootScope.audAudioPlayer.src !== objStreamURLs[$stateParams.streamId]) {
+
+                console.log('Setting new source');
+                $rootScope.audAudioPlayer.src = objStreamURLs[$stateParams.streamId];
+                $rootScope.audAudioPlayer.play();
+
+            }
+
+        } else {
+
+            $rootScope.audAudioPlayer = new Audio(objStreamURLs[$stateParams.streamId]);
+
+            $rootScope.audAudioPlayer.addEventListener('playing', function() {
+
+                console.log('Playback started!');
+                $rootScope.isPlaying = true;
+
+            });
+
+            $rootScope.audAudioPlayer.play();
+
+        }
+
+        $scope.streamId = $stateParams.streamId;
+        $scope.sContentClass = 'content-stream' + $stateParams.streamId;
 
     });
